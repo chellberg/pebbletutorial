def3cbecd0eed707af348f4514c3c31b6b75d220
Corin Chellberg <CorinChellberg@gmail.com>
2014-09-18 04:34:35 -0400

    working time

--- src/tutorial.c
+++ src/tutorial.c
@@ -3,13 +3,30 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  // create long-lived buffer to store current time
+  static char buffer[] = "00:00";
+  
+  // write time into buffer
+  if(clock_is_24h_style() == true) {
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    //   where string is - how long - what kind - where time is
+  } else {
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  // display buffer on TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
 static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50)); // point the pointer to the location of the newly-created TextLayer - modify in place
   
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  
-  text_layer_set_text(s_time_layer, "00:00");
   
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
@@ -22,10 +39,17 @@ static void main_window_unload(Window *window) { // destroy what you created in 
   text_layer_destroy(s_time_layer);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void init() {
   // point global pointer to newly-created window 
   s_main_window = window_create(); // presumably this returns the address of the Window
                                    // if that's how pointers work
+  
+  // subscribe to TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load, 
