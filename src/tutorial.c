b3ccf3a132be2d85ab0f42e62f66bbb6181a7f59
Corin Chellberg <CorinChellberg@gmail.com>
2014-10-16 00:40:07 -0400

    track and display initial time
    Note and display the time when the watchface was launched. This will be used to check the time-since-launch against a user-configurable reminder interval.

--- src/tutorial.c
+++ src/tutorial.c
@@ -1,29 +1,40 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+
 static TextLayer *s_time_layer;
+static TextLayer *s_initial_time_layer;
+
+static time_t *static_time;
+static time_t init_time;
 
 static void update_time() {
+  
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
+  struct tm *init_time = localtime(static_time);
   
   // create long-lived buffer to store current time
   static char buffer[] = "00:00";
+  static char initial_time_text[] = "00:00";
   
   // write time into buffer
   if(clock_is_24h_style() == true) {
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(initial_time_text, sizeof("00:00"), "%H:%M", init_time);
     //   where string is - how long - what kind - where time is
   } else {
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(initial_time_text, sizeof("00:00"), "%I:%M", init_time);
   }
   
   // display buffer on TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_initial_time_layer, initial_time_text);
 }
 
 static void main_window_load(Window *window) {
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50)); // point the pointer to the location of the newly-created TextLayer - modify in place
+  s_time_layer = text_layer_create(GRect(0, 10, 144, 50)); // point the pointer to the location of the newly-created TextLayer - modify in place
   
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
@@ -31,12 +42,24 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
-  // add s_time_layer as a child layer of window's root layer
+  s_initial_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  
+  text_layer_set_background_color(s_initial_time_layer, GColorClear);
+  text_layer_set_text_color(s_initial_time_layer, GColorBlack);
+  
+  text_layer_set_font(s_initial_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  text_layer_set_text_alignment(s_initial_time_layer, GTextAlignmentCenter);  // add s_time_layer as a child layer of window's root layer
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_initial_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-}
+  
+  init_time = time(NULL); // store time when watchface was launched
+  static_time = &init_time;
+} 
 
 static void main_window_unload(Window *window) { // destroy what you created in main_window_load
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_initial_time_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
